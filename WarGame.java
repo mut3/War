8124278b2581126cf1805854b78fd2cf699ff388
Will Barnwell <wwbarnwell@gmail.com>
2014-04-30 04:11:24 -0400

    restructured WarGame

--- WarGame.java
+++ WarGame.java
@@ -6,35 +6,21 @@ nerds use javaDocs
 */
 import java.util.ArrayList;
 public class WarGame
-{
-	//Lets write some pseudo code
-
-	//play
-		//if p1 has cards in deck
-			//flip top card
-		//elif p1 has cards in pile
-			//shuffle p1 deck and flip top card
-		//else p1 loses
-
-		//repeat if-elif-else block for p2
-
-		//compare cards
-		//if p1 card > p2 card
-			//p1 wins this draw and both cards are added to Winnings and they are marked as winners
-		//elif p2 card > p1 card
-			//p2 """"""
-		//else
-			//return a war starter
+{	//player Decks
+	Deck player1Deck;
+	Deck player2Deck;
+	//Cards for playing
+	Card p1Card;
+	Card p2Card;
+	//create winnings piles
+	ArrayList<Card> p1Winnings = new ArrayList<Card>();
+	ArrayList<Card> p2Winnings = new ArrayList<Card>();
+	//create neutral winnings Array
+	ArrayList<Card> winnings = new ArrayList<Card>();
 
 
-		//return Winnings AL and a 1 or 2 showing who won or 0 for war
-
-	
-
-
-
-	//main
-	public static void main(String[] args) 
+	//Constructor
+	public WarGame() 
 	{
 		//create a deck
 		Deck d = new Deck();
@@ -42,8 +28,8 @@ public class WarGame
 		d.shuffle();
 		//split it in half 
 		//call one half player1Deck and the other half player2Deck
-		Deck player1Deck = new Deck(d,0,26);
-		Deck player2Deck = new Deck(d,26,52);
+		player1Deck = new Deck(d,0,26);
+		player2Deck = new Deck(d,26,52);
 		
 		//debug prints for decks
     	/*int length1=d.cardsRemaining();
@@ -58,14 +44,42 @@ public class WarGame
 			System.out.println(player2Deck.getCard(i).toString());*/
 		//end debug prints
 
-		//create winnings piles
-		ArrayList<Card> p1Winnings = new ArrayList<Card>();
-		ArrayList<Card> p2Winnings = new ArrayList<Card>();
-		//create neutral winnings Array
-		ArrayList<Card> winnings = new ArrayList<Card>();
+		
+	
 		
 		
 	}
+
+	//Lets write some pseudo code
+
+	//play
+		//if p1 has cards in deck
+			//flip top card
+		//elif p1 has cards in pile
+			//shuffle p1 deck and flip top card
+		//else p1 loses
+
+		//repeat if-elif-else block for p2
+
+		//compare cards
+		//if p1 card > p2 card
+			//p1 wins this draw and both cards are added to Winnings 
+			//and they are marked as winners
+		//elif p2 card > p1 card
+			//p2 """"""
+		//else
+			//war starter
+
+
+		//return Winnings AL and a 1 or 2 showing who won or 0 for war
+
+	
+	//methods to implement based on GUI
+		//String getStatusText
+
+
+
+	
 		
 
 
