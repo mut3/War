86314d504ba20f70dd57aef35118e787a9cbc758
Will Barnwell <wwbarnwell@gmail.com>
2014-04-29 21:46:03 -0400

    wrote pseudo-code/comments outlining how the game will work

--- WarGame.java
+++ WarGame.java
@@ -7,5 +7,35 @@ nerds use javaDocs
 
 public class WarGame
 {
-	
+	//Lets write some pseudo code
+
+	//play
+		//if p1 has cards in deck
+			//flip top card
+		//elif p1 has cards in pile
+			//shuffle p1 deck and flip top card
+		//else p1 loses
+
+		//repeat if-elif-else block for p2
+
+		//compare cards
+		//if p1 card > p2 card
+			//p1 wins this draw and both cards are added to Winnings and they are marked as winners
+		//elif p2 card > p1 card
+			//p2 """"""
+		//else
+			//the original cards plus 1 card from the top of each deck are added to Winnings
+			//call play again
+
+
+		//return Winnings AL and a 1 or 2 showing who won
+
+
+	//main
+		//create a deck
+		//shuffle it
+		//split it in half
+		//call one half player1Deck and the other half player2Deck
+
+
 }
